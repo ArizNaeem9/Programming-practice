17d2bb10aa8c2de9ba781645b8de8036c8207059
ArizNaeem9 <ArizNaeem201@gmail.com>
2023-06-07 21:59:40 +0500

    fib series

--- practice/Fib series.java
+++ practice/Fib series.java
@@ -1,10 +1,8 @@
 package Edureka;
 public class Fibonacci {
 public static void main(String[] args) {
-//initializing the constants
 int n = 100, t1 = 0, t2 = 1;
 System.out.print("Upto " + n + ": ");
-//while loop to calculate fibonacci series upto n numbers
 while (t1<= n)
 {
 System.out.print(t1 + " + ");
