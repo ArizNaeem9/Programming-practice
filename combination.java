685126cd2ac231f259c454d3820e40f749551300
ArizNaeem9 <ArizNaeem201@gmail.com>
2023-06-12 17:51:01 +0500

    combination

---
+++ combination.java
@@ -0,0 +1,8 @@
+package Edureka;
+import java.util.Scanner;
+public class nprandncr {
+//calculating a factorial of a number
+public static int fact(int num)
+{
+int fact=1, i;
+for(i=1; i<=num; i++)
