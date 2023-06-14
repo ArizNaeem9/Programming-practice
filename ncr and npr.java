067a2ce73897c2426fdcacd62693e4c815dbdf71
ArizNaeem9 <ArizNaeem201@gmail.com>
2023-06-14 21:39:01 +0500

    ncr and npr

---
+++ ncr and npr.java
@@ -0,0 +1,17 @@
+fact = fact*i;
+}
+return fact;
+}
+public static void main(String args[])
+{
+int n, r;
+Scanner scan = new Scanner(System.in);
+System.out.print("Enter Value of n : ");
+n = scan.nextInt();
+System.out.print("Enter Value of r : ");
+r = scan.nextInt();
+// NCR and NPR of a number
+System.out.print("NCR = " +(fact(n)/(fact(n-r)*fact(r))));
+System.out.print("nNPR = " +(fact(n)/(fact(n-r))));
+}
+}
